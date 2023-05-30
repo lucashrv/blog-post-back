15c512a651194916fbf73b67cef5512b37eb0cda
lucashrv <lucas.hrv@outlook.com>
2023-05-29 23:01:26 -0300

    update logging

--- database/database.js
+++ database/database.js
@@ -8,7 +8,8 @@ const connection = new Sequelize(
     {
         host: 'localhost',
         dialect: 'mysql',
-        timezone: '-03:00'
+        timezone: '-03:00',
+        logging: false
     }
 )
 
